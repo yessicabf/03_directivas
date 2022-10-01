400f788b6714f1e0fc2c384eff840170722f6015
yessicabf <108480516+yessicabf@users.noreply.github.com>
2022-09-30 19:08:16 -0500

    03_directivas

---
+++ 03_directivas/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Universidad Tecnologica de Tehuacán</h1>
+    
+    <h3>Aplicaciones Web </h3>
+    <h4>Grupo 4A</h4>
+  <h1> Directivas en VUE</h1>
+  
+  <hr /> <!--Fragmentacion de texto    v-bind:src="texto" modificador o texto de la etiqueta*/-->
+  
+  <h2>v.text</h2>
+  <span v-text="'Hola desde la directiva de v-text'"></span>
+  <br>
+  <span v-text="'Hola desde la directiva de v-text'"></span>
+  <br>
+  <span v-text="titulo"></span> <!--Creacion de una clase-->
+  <br>
+  Esta es una trasmutacion de texto con una variable prop <br>
+  {{titulo}}
+  
+  
+  </div>
+  </template>
+  
+  
+  <script>
+  
+  export default{
+    data: () => ({   
+      titulo:"Hola desde Vue.js",
+    }),
+  }
+  
+  </script>
+  
+  <style scoped>
+  
+  div{
+    text-align: center;
+    margin: 0px 2rem;
+  
+  
+  }
+  </style>
